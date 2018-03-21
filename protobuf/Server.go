dbe4b5adc500571b6522be417d5e1c09ac2a37a8
Christian Wöhrle <christian.woehrle@googlemail.com>
2018-03-21 13:03:54 +0100

    first implementatation of a protobf client-server communication

--- protobuf/Server.go
+++ protobuf/Server.go
@@ -18,36 +18,33 @@ import (
 	"google.golang.org/grpc"
 )
 
-func main() {
-
-	go server()
-	time.Sleep(1 * time.Second)
-	context := context.Background()
-
-	clientConnection, err := grpc.Dial("localhost:8888", grpc.WithInsecure())
-	handleErr("Could not Dial grpc", err)
-	client := person.NewPersonServiceClient(clientConnection)
-	pn := person.Person_Name{Family: "woehrle", Personal: "pers"}
-	pe := person.Person_Email{Kind: "job", Address: "cw@gm.com"}
-	pes := []*person.Person_Email{&pe}
-	p := person.Person{Name: &pn, Email: pes}
-	clientEcho(context, client, &p)
-
-}
-
-func clientEcho(context context.Context, client person.PersonServiceClient, person *person.Person) {
-	person2, err := client.Echo(context, person, grpc.FailFast(true))
-	fmt.Println(person2, err)
-}
-
 type PersonService struct{}
 
 func (p PersonService) Echo(context context.Context, person *person.Person) (*person.Person, error) {
 	return person, nil
 }
 
-func server() {
+func main() {
+	go server()
+	time.Sleep(1 * time.Second)
+	clientEcho()
 
+}
+
+func clientEcho() {
+	clientConnection, err := grpc.Dial("localhost:8888", grpc.WithInsecure())
+	handleErr("Could not Dial grpc", err)
+	client := person.NewPersonServiceClient(clientConnection)
+
+	pn := person.Person_Name{Family: "woehrle", Personal: "pers"}
+	pe := person.Person_Email{Kind: "job", Address: "cw@gm.com"}
+	pes := []*person.Person_Email{&pe}
+	p := person.Person{Name: &pn, Email: pes}
+	person2, err := client.Echo(context.Background(), &p, grpc.FailFast(true))
+	fmt.Println(person2, err)
+}
+
+func server() {
 	srv := grpc.NewServer()
 	var pss PersonService
 	person.RegisterPersonServiceServer(srv, pss)
@@ -56,7 +53,6 @@ func server() {
 	listener, _ := net.ListenTCP("tcp", addr)
 	handleErr("Could not create Listener", err)
 	log.Fatal(srv.Serve(listener))
-
 }
 
 func handleErr(text string, err error) {
